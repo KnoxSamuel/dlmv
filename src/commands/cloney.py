86aa452dfbce2cc978a21528ce134b2e8211444d
Samuel Knox <knoxsa@oregonstate.edu>
2024-02-14 00:20:50 -0800

    bug: cloney feature throwing seg faults

--- src/commands/cloney.py
+++ src/commands/cloney.py
@@ -1,10 +1,23 @@
+import subprocess
+import sys
 import curses
 import os
 
 class Cloney:
-    def __init__(self) -> None:
-        pass
+    def __init__(self, repository_url: str, options: list = None) -> None:
+        self.repository_url = repository_url
+        self.options = options if options else []
+        
+    def clone(self) -> None:
+        command = ["git", "clone"] + self.options + [self.repository_url]
+        try:
+            subprocess.run(command, check=True)
+        except subprocess.CalledProcessError as e:
+            print(f"Error executing git clone: e", file=sys.stderr)
     
     '''
-    TODO: - [ ] Wrap git clone in class Cloney to pass options to git-clone(1) like so: `dlmv cloney --options <argument> <repository>`
+    TODO: - [ ] Wrap git clone in class Cloney to pass options to git-clone(1)
+                like so: `dlmv cloney --options <argument> <repository>`
+        
+        Integration with dlmv: Ensure that the Cloney class can be easily integrated with the rest of the dlmv tool, likely through a command-line argument that triggers the cloning feature.
     '''
--- src/main.py
+++ src/main.py
@@ -1,8 +1,25 @@
 import curses
 import os
 from utils.file_navigator import FileNavigator
+import argparse
+from commands.cloney import Cloney
+
+def parse_args():
+    prs = argparse.ArgumentParser(description='dlmv TUI/CLI tool')
+    prs.add_argument('--clone', help='Clone a repository using git', action='store_true')
+    prs.add_argument('repository_url', nargs='?', help='The URL of the repository to clone')
+    prs.add_argument('--options', nargs='*', help='Additional options for git clone')
+    return prs.parse_args()
 
 def main(stdscr):
+    if args.clone and args.repository_url:
+        # git clone if --clone is specified
+        options = args.options if args.options else []
+        cloney_instance = Cloney(args.repository_url, options)
+        cloney_instance.clone()
+        return  # no curses tui?
+
+    # Initialize curses interface if not cloning
     s = curses.initscr()
     curses.curs_set(0)
     sh, sw = s.getmaxyx()
@@ -11,4 +28,5 @@ def main(stdscr):
     file_navigator.navigate()
 
 if __name__ == "__main__":
-    curses.wrapper(main)
+    args = parse_args()
+    curses.wrapper(main, args)
