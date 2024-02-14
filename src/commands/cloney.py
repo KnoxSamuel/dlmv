86990bc95b24db14ccd795ceb81d6f4367e18601
Samuel Knox <knoxsa@oregonstate.edu>
2024-02-13 20:35:25 -0800

    working on class Cloney for git clone wrapper

---
+++ src/commands/cloney.py
@@ -0,0 +1,10 @@
+import curses
+import os
+
+class Cloney:
+    def __init__(self) -> None:
+        pass
+    
+    '''
+    TODO: - [ ] Wrap git clone in class Cloney to pass options to git-clone(1) like so: `dlmv cloney --options <argument> <repository>`
+    '''
