640a75240aedd0b1556e4481ced500ac9e6b9e56
Samuel Knox <knoxsa@oregonstate.edu>
2024-02-20 14:49:49 -0800

    fix: append repo name to destination path so git doesn't throw non-empty dir errors

--- src/commands/cloney.py
+++ src/commands/cloney.py
@@ -12,9 +12,13 @@ class Cloney:
         self.dest = dest
 
     def clone(self) -> None:
-        command = ["git", "clone"] + self.options + [self.repo_url]
+        repo_name = self.repo_url.split('/')[-1].split('.')[0] # parse project name for dest path
         if self.dest:
-            command.append(self.dest)
+            final_dest = os.path.join(self.dest, repo_name)
+        else:
+            final_dest = repo_name
+        
+        command = ["git", "clone"] + self.options + [self.repo_url, final_dest]
         try:
             subprocess.run(command, check=True)
         except subprocess.CalledProcessError as e:
