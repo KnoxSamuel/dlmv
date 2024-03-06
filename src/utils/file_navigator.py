f1622594719827269d4d40e60182ca3635b04dec
Samuel Knox <knoxsa@oregonstate.edu>
2024-03-05 22:41:30 -0800

    add scrolling and resizing handling

--- src/utils/file_navigator.py
+++ src/utils/file_navigator.py
@@ -13,23 +13,32 @@ class FileNavigator:
         self.refresh_files()
         self.height, self.width = stdscr.getmaxyx()
         self.window = curses.newwin(self.height - 2, self.width - 2, 1, 1) # Window border
+        self.scroll_pos = 0
 
     def refresh_files(self):
         self.files = os.listdir(self.current_path)
 
     def draw(self):
+        if self.height < 4 or self.width < 20:
+            self.stdscr.clear()
+            self.stdscr.addstr(0, 0, "Terminal too small.", curses.A_REVERSE)
+            self.stdscr.refresh()
+            return
+
         self.stdscr.clear()
         self.window.clear()
         self.stdscr.box()
-        
+
         # Display the current path above border
         self.stdscr.addstr(0, 1, f" Current Path: {self.current_path} ", curses.A_REVERSE)
 
-        for index, file in enumerate(self.files[:self.height - 4]):
-            if index == self.selected_index:
-                self.window.addstr(index + 1, 1, file, curses.A_REVERSE)
+        visible_files = self.files[self.scroll_pos:self.scroll_pos + self.height - 4]
+
+        for i, file in enumerate(visible_files):
+            if self.selected_index == self.scroll_pos + i:
+                self.window.addstr(i+1, 1, file, curses.A_REVERSE)
             else:
-                self.window.addstr(index + 1, 1, file)
+                self.window.addstr(i+1, 1, file)
 
         self.stdscr.refresh()
         self.window.refresh()
@@ -77,10 +86,21 @@ class FileNavigator:
             self.draw()
             key = self.stdscr.getch()
 
+            max_scroll = max(0, len(self.files) - (self.height - 4))
+            max_index = len(self.files) - 1
+
             if key in [curses.KEY_UP, ord('k')]:
-                self.selected_index = max(0, self.selected_index - 1)
+                if self.selected_index > 0:
+                    self.selected_index -= 1
+                if self.selected_index < self.scroll_pos:
+                    self.scroll_pos = max(0, self.scroll_pos - 1)
+            
             elif key in [curses.KEY_DOWN, ord('j')]:
-                self.selected_index = min(len(self.files) - 1, self.selected_index + 1)
+                if self.selected_index < max_index:
+                    self.selected_index += 1
+                if self.selected_index >= self.scroll_pos + self.height - 4:
+                    self.scroll_pos = min(max_scroll, self.scroll_pos + 1)
+            
             elif key in [curses.KEY_RIGHT, ord('l'), curses.KEY_ENTER, 10]:
                 selected_file = self.files[self.selected_index]
                 new_path = os.path.join(self.current_path, selected_file)
@@ -88,13 +108,17 @@ class FileNavigator:
                     self.current_path = new_path
                     self.refresh_files()
                     self.selected_index = 0
+            
             elif key in [curses.KEY_LEFT, ord('h')]:
                 if self.current_path != '/':
                     self.current_path = os.path.dirname(self.current_path)
                     self.refresh_files()
                     self.selected_index = 0
+            
             elif key == ord('q'):
+                curses.endwin()
                 break
+            
             elif key == ord('c') and self.cloney_instance:
                 self.stdscr.addstr(self.height - 1, 1, "Cloning... Please wait.", curses.A_REVERSE)
                 self.stdscr.refresh()
@@ -102,5 +126,15 @@ class FileNavigator:
                 self.cloney_instance.clone()
                 self.stdscr.addstr(self.height - 1, 1, "Cloning complete. Press any key to continue...", curses.A_REVERSE)
                 self.stdscr.getch()
+                self.refresh_files()
+            
             elif key == ord('p'):
                 self.prompt_custom_path()
+            
+            elif key == curses.KEY_RESIZE:
+                self.height, self.width = self.stdscr.getmaxyx()
+                self.window.resize(self.height - 2, self.width - 2)
+                if self.height < 4 or self.width < 20:
+                    continue
+
+
